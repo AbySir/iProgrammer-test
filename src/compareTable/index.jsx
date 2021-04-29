1e234c128b5801d9ece62b6978a3c58c99007e1f
Abhinav Gupta <abhinavit3002@gmail.com>
2021-04-29 23:47:52 +0530

    Push v1

--- src/compareTable/index.jsx
+++ src/compareTable/index.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 
 const CompareTable = (props) => {
     const { selectedPhotos } = props;
+
+    const renderTableBody = () => {
+        return(selectedPhotos.map((picDetails, i) => <tr key={picDetails.id}>
+            <td>Photo {i+1}</td>
+            <td>{picDetails.id}</td>
+            <td>{picDetails.thumbnailUrl}</td>
+            <td>{picDetails.title}</td>
+        </tr>))
+    }
+
     return (<div>
         <table>
             <tr>
-            <th colspan="4">COMPARISON TABLE</th>
+            <th colSpan="4">COMPARISON TABLE</th>
             </tr>
             <tr>
                 <th></th>
@@ -13,12 +23,7 @@ const CompareTable = (props) => {
                 <th>URL</th>
                 <th>Title</th>
             </tr>
-            <tr>
-                <td>Photo 1</td>
-                <td>Alfreds Futterkiste</td>
-                <td>Maria Anders</td>
-                <td>Germany</td>
-            </tr>
+            {renderTableBody()}
         </table>
   </div>);
 }
--- src/photos/index.jsx
+++ src/photos/index.jsx
@@ -5,33 +5,49 @@ import CompareTable from '../compareTable';
 const Photos = () => {
 
     const [ photosList, setPhotosList ] = useState([]);
-    const [ selectedphotoList, setSelectedphotoList ] = useState([]);
+    const [ selectedPhotoList, setSelectedPhotoList ] = useState([]);
 
-    useEffect(async () => {
-        const photosData = await axios.get('https://jsonplaceholder.typicode.com/photos');
+    const getPhotoData = async() => {
+        let photosData = await axios.get('https://jsonplaceholder.typicode.com/photos');
+        photosData['isSelected'] = false;
         setPhotosList(photosData.data);
+    }
+
+    useEffect(() => {
+        getPhotoData();
     }, []);
 
     const setPhotoData = (selectedPhoto) => {
-        // compare and update data
-        // setSelectedphotoList()
+        const tempSelectedList = [...selectedPhotoList];
+        const tempPhotoList = [...photosList];
+        if(selectedPhoto['isSelected']){
+            let Ind = tempSelectedList.findIndex((picData) => picData.id === selectedPhoto.id);
+            tempSelectedList.splice(Ind, 1);
+        } else {
+            tempSelectedList.push(selectedPhoto)
+        }
+        selectedPhoto['isSelected'] = !selectedPhoto['isSelected'];
+        let picIndex = tempPhotoList.findIndex((picData) => picData.id === selectedPhoto.id);
+        tempPhotoList.splice(picIndex, 1, selectedPhoto);
+        setSelectedPhotoList(tempSelectedList)
+        setPhotosList(tempPhotoList);
     }
 
     const renderGrid = () => {
         return photosList.map((picDetails) => (
-            <div key={picDetails.id}>
-                <img src={picDetails.thumbnailUrl} alt="image"></img>
+            <div key={picDetails.id} className="photos">
+                <img src={picDetails.thumbnailUrl} alt={`image-${picDetails.id}`}></img>
                 <div>{picDetails.title}</div>
                 <div>{picDetails.id}</div>
                 <div>{picDetails.url}</div>
-                <button onClick={() => setPhotoData(picDetails)}>Compare</button>
+                <button onClick={() => setPhotoData(picDetails)}>{picDetails.isSelected ? 'Remove' : 'Compare'}</button>
             </div>
         ));
     }
 
-    return (<div>
+    return (<div className="photo-container">
         {photosList.length > 0 && renderGrid()}
-        {selectedphotoList.length > 0 || <CompareTable selectedPhotos={selectedphotoList} />}
+        {selectedPhotoList.length > 0 && <CompareTable selectedPhotos={selectedPhotoList} />}
     </div>);
 }
 
